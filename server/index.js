fdd365a89f9543213b988b0281c534fb044ed8b7
Carson Saunders <carsonsaundersalt@gmail.com>
2021-07-19 16:39:06 -0600

    Created express server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,15 @@
 const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+app.get('/:name', (req, res) => {
+  const name = req.params.name;
+  res.send(`Hello ${name}`);
+});
+
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
