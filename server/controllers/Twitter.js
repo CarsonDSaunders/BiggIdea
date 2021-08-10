39a472b21ed811662583ab6b4908d4147e88269e
Carson Saunders <73197574+CarsonDSaunders@users.noreply.github.com>
2021-08-10 13:34:39 -0600

    Update Twitter.js

--- server/controllers/Twitter.js
+++ server/controllers/Twitter.js
@@ -4,11 +4,11 @@ require('dotenv').config();
 
 const client = new Twitter({
     // consumer_key: `${process.env.TWITTER_CONSUMER_TOKEN}`,
-    consumer_key: `q9YYaImmvaFi4Xz0XAMWJrh9Y`,
+    consumer_key: ``,
     // consumer_secret: `${process.env.TWITTER_CONSUMER_SECRET}`,
-    consumer_secret: `ARLa83vvop2MpRF2WGnM10KvDdrTDXUA73EvOw97noG23vXheO`,
+    consumer_secret: ``,
     // bearer_token: `${process.env.TWITTER_BEARER_TOKEN}`
-    bearer_token: `AAAAAAAAAAAAAAAAAAAAAFHdSQEAAAAAv1ENV%2BW7kaJ%2FFRjEKKv8ljNp2Y8%3Dd1vfKuyrKsKXr2LagvSqndlKwC8XuzZTfUOcljx5ZFMpmJhhI4`
+    bearer_token: ``
   });
 
 module.exports = {
